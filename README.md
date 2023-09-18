39778feca91dd08396c79c4d90c86aadb01f1a67
dudasso <143518772+dudasso@users.noreply.github.com>
2023-09-18 09:29:25 -0300

    adicionando correção, aparece a imagem de gatinhos

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # atividadepontoextra
 
 escolhi CATAAS 😹 🙀
+
+a aplicação consiste em uma pagina que toda vez que é reiniciada aparece um gato diferente
--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+const content = document.getElementById("content")
+
 fetch("https://cataas.com/cat")
-    .then(res => res.blob())
-    .then(res => {
-       const url = URL.createObjectURL(res)
+     .then(res => res.blob())
+     .then(res => {
+         const url = URL.createObjectURL(res)
          const img = document.createElement("img");
-        img.src = url
+         img.src = url
          content.appendChild(img)
      })
      .catch(err => {
